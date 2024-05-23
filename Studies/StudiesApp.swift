d6bba840d11f4d6da343a11b7eb38d7e165c6100
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-05-23 09:56:54 -0300

    Initial Commit

---
+++ Studies/StudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudiesApp.swift
+//  Studies
+//
+//  Created by Layza Maria Rodrigues Carneiro on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
